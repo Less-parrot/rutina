f9800f12990791c561030a8cfa4332aae6144e1c
Less-parrot <less100100@gmail.com>
2023-12-02 12:46:25 -0500

    Aguste de campos de Días de la semana

--- app/src/main/java/com/year/rutina/MainActivity.kt
+++ app/src/main/java/com/year/rutina/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.year.rutina
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -38,13 +50,53 @@ fun NavHostController() {
 
 @Composable
 fun MainScreen() {
+    val context = LocalContext.current
     RutinaTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background //color de fondo
         ) {
             Box(Modifier.fillMaxSize()) {
-                BoxWeek(day = "Lunes", day2 = "Martes", color = Color.Blue)
+                Column(Modifier.fillMaxSize()) {
+                    BoxWeek(
+                        day = "Lunes",
+                        day2 = "Martes",
+                        color = Color.Green,
+                        color2 = Color.Red,
+                        onClick = { Toast.makeText(context, "Lunes", Toast.LENGTH_SHORT).show()},
+                        onClick2 = { Toast.makeText(context, "Martes", Toast.LENGTH_SHORT).show()}
+
+
+                    )
+                    BoxWeek(
+                        day = "Miercoles",
+                        day2 = "Jueves",
+                        color = Color.Magenta,
+                        color2 = Color.Gray,
+                        onClick = { Toast.makeText(context, "Miercoles", Toast.LENGTH_SHORT).show()},
+                        onClick2 = { Toast.makeText(context, "Jueves", Toast.LENGTH_SHORT).show()}
+
+
+                    )
+                    BoxWeek(
+                        day = "Viernes",
+                        day2 = "Sábado",
+                        color = Color.DarkGray,
+                        color2 = Color.Red,
+                        onClick = {Toast.makeText(context, "Viernes", Toast.LENGTH_SHORT).show()},
+                        onClick2 = {Toast.makeText(context, "Sábado", Toast.LENGTH_SHORT).show()}
+                    )
+                    Box (
+                        Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                            .background(Color.Magenta)
+                            .clickable { Toast.makeText(context, "Domingo", Toast.LENGTH_SHORT).show() },
+                        Alignment.Center
+                    ){
+                        Text(text = "Domingo")
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/year/rutina/views/ViewMain.kt
+++ app/src/main/java/com/year/rutina/views/ViewMain.kt
@@ -1,38 +1,63 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
 package com.year.rutina.views
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun BoxWeek(day: String, day2: String, color: Color) {
-    Row {
-        Box(Modifier.fillMaxWidth(0.5f)) {
-            Surface(modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.3f),
-                color = color)
-            {
-                Text(text = day)
-            }
+fun BoxWeek(
+    day: String,
+    day2: String,
+    color: Color,
+    color2: Color,
+    onClick: () -> Unit,
+    onClick2: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(200.dp)
+                .background(color)
+                .clickable { onClick() },
+            Alignment.Center
+
+        ) {
+            Text(text = day)
         }
 
-        Box(Modifier.fillMaxWidth()) {
-            Surface(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.3F),
-                color = color
-            ) {
-                Text(text = day2)
-            }
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(200.dp)
+                .background(color2)
+                .clickable { onClick2() },
+            Alignment.Center
 
+        ) {
+            Text(text = day2)
         }
     }
 }
